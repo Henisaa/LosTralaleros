5875e0b9e19f4410d34bf05c17d08070dca39297
Henry <enriquenunez9996@hotmail.com>
2025-11-02 16:16:13 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,31 +7,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
-<<<<<<< HEAD
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly!!.</li>
-        </ol>
-=======
   const featuredProducts = products.slice(0, 9);
->>>>>>> enrique
 
   return (
     <>
@@ -50,13 +26,12 @@ export default function Home() {
         </p>
       </section>
 
-      {/* Reemplazamos el <div> por el componente <Carousel> */}
       <Carousel fade style={{ maxWidth: "100%", margin: "0 auto" }}>
         <Carousel.Item style={{ height: "400px", backgroundColor: "#333" }}>
           <Image
             src="/SilksongLogo.jpg"
             fill
-            style={{ objectFit: "cover" }} // Usamos 'fill' y 'style'
+            style={{ objectFit: "cover" }}
             className="d-block w-100"
             alt="Hornet"
             priority={true}
@@ -80,7 +55,7 @@ export default function Home() {
           <Image
             src="/DoomDarkAges.png"
             fill
-            style={{ objectFit: "cover" }} // Usamos 'fill' y 'style'
+            style={{ objectFit: "cover" }}
             className="d-block w-100"
             alt="Doomslayer"
           />
@@ -103,7 +78,7 @@ export default function Home() {
           <Image
             src="/TheBattleCatsLogo.png"
             fill
-            style={{ objectFit: "cover" }} // Usamos 'fill' y 'style'
+            style={{ objectFit: "cover" }}
             className="d-block w-100"
             alt="Battlecats"
           />
@@ -133,7 +108,7 @@ export default function Home() {
           ))}
         </div>
         <div className="text-center mt-5 mb-4">
-          <Link href="/main/productos" legacyBehavior>
+          <Link href="/productos" legacyBehavior>
             <a className="btn btn-primary btn-lg">Ver todo el catálogo</a>
           </Link>
         </div>
@@ -141,3 +116,4 @@ export default function Home() {
     </>
   );
 }
+
